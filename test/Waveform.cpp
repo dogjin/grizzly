1f1fd41ec2e4d63ebbe34919e96a37630564a04f
Stijn Frishert <info@stijnfrishert.com>
2017-05-12 15:47:30 +0200

    Fixed waveform test

--- test/Waveform.cpp
+++ test/Waveform.cpp
@@ -11,32 +11,32 @@ TEST_CASE("Waveform")
     {
         SECTION("bipolar")
         {
-            CHECK(generateSine<float>(-0.25) == Approx(-1));
-            CHECK(generateSine<float>(0) == Approx(0));
-            CHECK(generateSine<float>(0.125) == Approx(0.7071067812));
-            CHECK(generateSine<float>(0.25) == Approx(1));
-            CHECK(generateSine<float>(0.375) == Approx(0.7071067812));
-            CHECK(generateSine<float>(0.5) == Approx(0));
-            CHECK(generateSine<float>(0.625) == Approx(-0.7071067812));
-            CHECK(generateSine<float>(0.75) == Approx(-1));
-            CHECK(generateSine<float>(0.875) == Approx(-0.7071067812));
-            CHECK(generateSine<float>(1) == Approx(0));
-            CHECK(generateSine<float>(1.25) == Approx(1));
+            CHECK(generateBipolarSine<float>(-0.25) == Approx(-1));
+            CHECK(generateBipolarSine<float>(0) == Approx(0));
+            CHECK(generateBipolarSine<float>(0.125) == Approx(0.7071067812));
+            CHECK(generateBipolarSine<float>(0.25) == Approx(1));
+            CHECK(generateBipolarSine<float>(0.375) == Approx(0.7071067812));
+            CHECK(generateBipolarSine<float>(0.5) == Approx(0));
+            CHECK(generateBipolarSine<float>(0.625) == Approx(-0.7071067812));
+            CHECK(generateBipolarSine<float>(0.75) == Approx(-1));
+            CHECK(generateBipolarSine<float>(0.875) == Approx(-0.7071067812));
+            CHECK(generateBipolarSine<float>(1) == Approx(0));
+            CHECK(generateBipolarSine<float>(1.25) == Approx(1));
         }
 
         SECTION("unipolar")
         {
-            CHECK(generateUnipolarSine<float>(-0.25) == Approx(0));
-            CHECK(generateUnipolarSine<float>(0) == Approx(0.5));
-            CHECK(generateUnipolarSine<float>(0.125) == Approx(0.85355339059328));
-            CHECK(generateUnipolarSine<float>(0.25) == Approx(1));
+            CHECK(generateUnipolarSine<float>(-0.25) == Approx(0.5));
+            CHECK(generateUnipolarSine<float>(0) == Approx(0));
+            CHECK(generateUnipolarSine<float>(0.125) == Approx(0.14644660940672));
+            CHECK(generateUnipolarSine<float>(0.25) == Approx(0.5));
             CHECK(generateUnipolarSine<float>(0.375) == Approx(0.85355339059328));
-            CHECK(generateUnipolarSine<float>(0.5) == Approx(0.5));
-            CHECK(generateUnipolarSine<float>(0.625) == Approx(0.14644660940672));
-            CHECK(generateUnipolarSine<float>(0.75) == Approx(0));
-            CHECK(generateUnipolarSine<float>(0.875) == Approx(0.14644660940672));
-            CHECK(generateUnipolarSine<float>(1) == Approx(0.5));
-            CHECK(generateUnipolarSine<float>(1.25) == Approx(1));
+            CHECK(generateUnipolarSine<float>(0.5) == Approx(1));
+            CHECK(generateUnipolarSine<float>(0.625) == Approx(0.85355339059328));
+            CHECK(generateUnipolarSine<float>(0.75) == Approx(0.5));
+            CHECK(generateUnipolarSine<float>(0.875) == Approx(00.14644660940672));
+            CHECK(generateUnipolarSine<float>(1) == Approx(0));
+            CHECK(generateUnipolarSine<float>(1.25) == Approx(0.5));
         }
     }
     
@@ -44,17 +44,17 @@ TEST_CASE("Waveform")
     {
         SECTION("bipolar")
         {
-            CHECK(generateSaw<float>(-0.25) == Approx(-0.5));
-            CHECK(generateSaw<float>(0) == Approx(0));
-            CHECK(generateSaw<float>(0.125) == Approx(0.25));
-            CHECK(generateSaw<float>(0.25) == Approx(0.5));
-            CHECK(generateSaw<float>(0.375) == Approx(0.75));
-            CHECK(generateSaw<float>(0.5) == Approx(-1));
-            CHECK(generateSaw<float>(0.625) == Approx(-0.75));
-            CHECK(generateSaw<float>(0.75) == Approx(-0.5));
-            CHECK(generateSaw<float>(0.875) == Approx(-0.25));
-            CHECK(generateSaw<float>(1) == Approx(0));
-            CHECK(generateSaw<float>(1.25) == Approx(0.5));
+            CHECK(generateBipolarSaw<float>(-0.25) == Approx(-0.5));
+            CHECK(generateBipolarSaw<float>(0) == Approx(0));
+            CHECK(generateBipolarSaw<float>(0.125) == Approx(0.25));
+            CHECK(generateBipolarSaw<float>(0.25) == Approx(0.5));
+            CHECK(generateBipolarSaw<float>(0.375) == Approx(0.75));
+            CHECK(generateBipolarSaw<float>(0.5) == Approx(-1));
+            CHECK(generateBipolarSaw<float>(0.625) == Approx(-0.75));
+            CHECK(generateBipolarSaw<float>(0.75) == Approx(-0.5));
+            CHECK(generateBipolarSaw<float>(0.875) == Approx(-0.25));
+            CHECK(generateBipolarSaw<float>(1) == Approx(0));
+            CHECK(generateBipolarSaw<float>(1.25) == Approx(0.5));
         }
 
         SECTION("unipolar")
@@ -77,17 +77,17 @@ TEST_CASE("Waveform")
     {
         SECTION("bipolar")
         {
-            CHECK(generateTriangle<float>(-0.25) == Approx(-1));
-            CHECK(generateTriangle<float>(0) == Approx(0));
-            CHECK(generateTriangle<float>(0.125) == Approx(0.5));
-            CHECK(generateTriangle<float>(0.25) == Approx(1));
-            CHECK(generateTriangle<float>(0.375) == Approx(0.5));
-            CHECK(generateTriangle<float>(0.5) == Approx(0));
-            CHECK(generateTriangle<float>(0.625) == Approx(-0.5));
-            CHECK(generateTriangle<float>(0.75) == Approx(-1));
-            CHECK(generateTriangle<float>(0.875) == Approx(-0.5));
-            CHECK(generateTriangle<float>(1) == Approx(0));
-            CHECK(generateTriangle<float>(1.25) == Approx(1));
+            CHECK(generateBipolarTriangle<float>(-0.25) == Approx(-1));
+            CHECK(generateBipolarTriangle<float>(0) == Approx(0));
+            CHECK(generateBipolarTriangle<float>(0.125) == Approx(0.5));
+            CHECK(generateBipolarTriangle<float>(0.25) == Approx(1));
+            CHECK(generateBipolarTriangle<float>(0.375) == Approx(0.5));
+            CHECK(generateBipolarTriangle<float>(0.5) == Approx(0));
+            CHECK(generateBipolarTriangle<float>(0.625) == Approx(-0.5));
+            CHECK(generateBipolarTriangle<float>(0.75) == Approx(-1));
+            CHECK(generateBipolarTriangle<float>(0.875) == Approx(-0.5));
+            CHECK(generateBipolarTriangle<float>(1) == Approx(0));
+            CHECK(generateBipolarTriangle<float>(1.25) == Approx(1));
         }
 
         SECTION("unipolar")
@@ -112,68 +112,68 @@ TEST_CASE("Waveform")
         {
             SECTION("pulse width 50%")
             {
-                CHECK(generateSquare<float>(-0.25, 0.5) == -1);
-                CHECK(generateSquare<float>(0, 0.5) == 1);
-                CHECK(generateSquare<float>(0.25, 0.5) == 1);
-                CHECK(generateSquare<float>(0.5, 0.5) == -1);
-                CHECK(generateSquare<float>(0.75, 0.5) == -1);
-                CHECK(generateSquare<float>(1, 0.5) == 1);
-                CHECK(generateSquare<float>(1.25, 0.5) == 1);
+                CHECK(generateSquare<float>(-0.25, 0.5, -1, 1) == -1);
+                CHECK(generateSquare<float>(0, 0.5, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 0.5, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 0.5, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.75, 0.5, -1, 1) == -1);
+                CHECK(generateSquare<float>(1, 0.5, -1, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 0.5, -1, 1) == 1);
             }
             
             SECTION("pulse width 25%")
             {
-                CHECK(generateSquare<float>(-0.25, 0.25) == -1);
-                CHECK(generateSquare<float>(0, 0.25) == 1);
-                CHECK(generateSquare<float>(0.25, 0.25) == -1);
-                CHECK(generateSquare<float>(0.5, 0.25) == -1);
-                CHECK(generateSquare<float>(0.75, 0.25) == -1);
-                CHECK(generateSquare<float>(1, 0.25) == 1);
-                CHECK(generateSquare<float>(1.25, 0.25) == -1);
+                CHECK(generateSquare<float>(-0.25, 0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0, 0.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.5, 0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.75, 0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(1, 0.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 0.25, -1, 1) == -1);
             }
             
             SECTION("pulse width 0%")
             {
-                CHECK(generateSquare<float>(-0.25, 0) == -1);
-                CHECK(generateSquare<float>(0, 0) == -1);
-                CHECK(generateSquare<float>(0.25, 0) == -1);
-                CHECK(generateSquare<float>(0.5, 0) == -1);
-                CHECK(generateSquare<float>(0.75, 0) == -1);
-                CHECK(generateSquare<float>(1, 0) == -1);
-                CHECK(generateSquare<float>(1.25, 0) == -1);
+                CHECK(generateSquare<float>(-0.25, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(0, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.25, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.5, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.75, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(1, 0, -1, 1) == -1);
+                CHECK(generateSquare<float>(1.25, 0, -1, 1) == -1);
             }
             
             SECTION("pulse width 100%")
             {
-                CHECK(generateSquare<float>(-0.25, 1) == 1);
-                CHECK(generateSquare<float>(0, 1) == 1);
-                CHECK(generateSquare<float>(0.25, 1) == 1);
-                CHECK(generateSquare<float>(0.5, 1) == 1);
-                CHECK(generateSquare<float>(0.75, 1) == 1);
-                CHECK(generateSquare<float>(1, 1) == 1);
-                CHECK(generateSquare<float>(1.25, 1) == 1);
+                CHECK(generateSquare<float>(-0.25, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(0, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.75, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(1, 1, -1, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 1, -1, 1) == 1);
             }
 
             SECTION("pulse width -25%")
             {
-                CHECK(generateSquare<float>(-0.25, -0.25) == -1);
-                CHECK(generateSquare<float>(0, -0.25) == -1);
-                CHECK(generateSquare<float>(0.25, -0.25) == -1);
-                CHECK(generateSquare<float>(0.5, -0.25) == -1);
-                CHECK(generateSquare<float>(0.75, -0.25) == -1);
-                CHECK(generateSquare<float>(1, -0.25) == -1);
-                CHECK(generateSquare<float>(1.25, -0.25) == -1);
+                CHECK(generateSquare<float>(-0.25, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.25, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.5, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(0.75, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(1, -0.25, -1, 1) == -1);
+                CHECK(generateSquare<float>(1.25, -0.25, -1, 1) == -1);
             }
             
             SECTION("pulse width 125%")
             {
-                CHECK(generateSquare<float>(-0.25, 1.25) == 1);
-                CHECK(generateSquare<float>(0, 1.25) == 1);
-                CHECK(generateSquare<float>(0.25, 1.25) == 1);
-                CHECK(generateSquare<float>(0.5, 1.25) == 1);
-                CHECK(generateSquare<float>(0.75, 1.25) == 1);
-                CHECK(generateSquare<float>(1, 1.25) == 1);
-                CHECK(generateSquare<float>(1.25, 1.25) == 1);
+                CHECK(generateSquare<float>(-0.25, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(0, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(0.75, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(1, 1.25, -1, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 1.25, -1, 1) == 1);
             }
         }
         
@@ -181,68 +181,68 @@ TEST_CASE("Waveform")
         {
             SECTION("pulse width 50%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, 0.5) == 0);
-                CHECK(generateUnipolarSquare<float>(0, 0.5) == 1);
-                CHECK(generateUnipolarSquare<float>(0.25, 0.5) == 1);
-                CHECK(generateUnipolarSquare<float>(0.5, 0.5) == 0);
-                CHECK(generateUnipolarSquare<float>(0.75, 0.5) == 0);
-                CHECK(generateUnipolarSquare<float>(1, 0.5) == 1);
-                CHECK(generateUnipolarSquare<float>(1.25, 0.5) == 1);
+                CHECK(generateSquare<float>(-0.25, 0.5, 0, 1) == 0);
+                CHECK(generateSquare<float>(0, 0.5, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 0.5, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 0.5, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.75, 0.5, 0, 1) == 0);
+                CHECK(generateSquare<float>(1, 0.5, 0, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 0.5, 0, 1) == 1);
             }
             
             SECTION("pulse width 25%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, 0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0, 0.25) == 1);
-                CHECK(generateUnipolarSquare<float>(0.25, 0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0.5, 0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0.75, 0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(1, 0.25) == 1);
-                CHECK(generateUnipolarSquare<float>(1.25, 0.25) == 0);
+                CHECK(generateSquare<float>(-0.25, 0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0, 0.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.5, 0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.75, 0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(1, 0.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 0.25, 0, 1) == 0);
             }
             
             SECTION("pulse width 0%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(0, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(0.25, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(0.5, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(0.75, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(1, 0) == 0);
-                CHECK(generateUnipolarSquare<float>(1.25, 0) == 0);
+                CHECK(generateSquare<float>(-0.25, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(0, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.25, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.5, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.75, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(1, 0, 0, 1) == 0);
+                CHECK(generateSquare<float>(1.25, 0, 0, 1) == 0);
             }
             
             SECTION("pulse width 100%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(0, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(0.25, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(0.5, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(0.75, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(1, 1) == 1);
-                CHECK(generateUnipolarSquare<float>(1.25, 1) == 1);
+                CHECK(generateSquare<float>(-0.25, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(0, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.75, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(1, 1, 0, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 1, 0, 1) == 1);
             }
 
             SECTION("pulse width -25%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0.25, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0.5, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(0.75, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(1, -0.25) == 0);
-                CHECK(generateUnipolarSquare<float>(1.25, -0.25) == 0);
+                CHECK(generateSquare<float>(-0.25, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.25, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.5, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(0.75, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(1, -0.25, 0, 1) == 0);
+                CHECK(generateSquare<float>(1.25, -0.25, 0, 1) == 0);
             }
             
             SECTION("pulse width 125%")
             {
-                CHECK(generateUnipolarSquare<float>(-0.25, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(0, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(0.25, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(0.5, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(0.75, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(1, 1.25) == 1);
-                CHECK(generateUnipolarSquare<float>(1.25, 1.25) == 1);
+                CHECK(generateSquare<float>(-0.25, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(0, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.25, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.5, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(0.75, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(1, 1.25, 0, 1) == 1);
+                CHECK(generateSquare<float>(1.25, 1.25, 0, 1) == 1);
             }
         }
     }
