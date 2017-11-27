08619470cb7b6b1d80ff2875fdcd772c10b9499c
Milan <vandermeer.milan@gmail.com>
2017-11-27 16:52:37 +0100

    added some comments

--- decibel_conversion.hpp
+++ decibel_conversion.hpp
@@ -1,12 +1,59 @@
-//
-//  decibell_conversion.hpp
-//  Grizzly
-//
-//  Created by Milan van der Meer on 24/11/2017.
-//
+/*
+ 
+ This file is a part of Grizzly, a modern C++ library for digital signal
+ processing. See https://github.com/moditone/grizzly for more information.
+ 
+ Copyright (C) 2016-2018 Moditone <info@moditone.com>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ 
+ --------------------------------------------------------------------
+ 
+ If you would like to use Grizzly for commercial or closed-source
+ purposes, please contact us for a commercial license.
+ 
+ */
 
-#ifndef decibell_conversion_h
-#define decibell_conversion_h
+#pragma once
 
+#include <cmath>
+#include <limits>
 
-#endif /* decibell_conversion_h */
+namespace dsp
+{
+    /*! @brief Convert linear amplitude to deciBel
+     *
+     */
+    template <typename T>
+    T amplitudeToDecibel(T value)
+    {
+        // get the smallest possible positive value for T
+        const T epsilon = std::numeric_limits<T>::min();
+        
+        // if the value is lower, use the epsilon
+        if (value < epsilon)
+            value = epsilon;
+        
+        return 20 * std::log10(value);
+    }
+    
+    /*! @brief Convert deciBel to linear amplitude
+     *
+     */
+    template <typename T>
+    T decibelToAmplitude(T value)
+    {
+        return std::pow(10, value * 0.05);
+    }
+}
